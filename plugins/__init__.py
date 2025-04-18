9adca70de7292df8d7b9e7b9130006c3e527359d
blandk77 <alexags1983@gmail.com>
2025-04-18 13:59:08 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -7,13 +7,14 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 from cloudscraper import create_scraper
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import Config, COLLECTION_NAME, VERIFY_EXPIRE, SHORTLINK_API, SHORTLINK_SITE, PREMIUM_USERS, DUAL_PROTECTION
+from config import Config, COLLECTION_NAME, VERIFY_EXPIRE, SHORTLINK_API, SHORTLINK_SITE, PREMIUM_USERS
 
-DATABASE_URL = Config.DB_URL
+DATABASE_URL = Config.DB_URI
 
 verify_dict = {}
 missing=[v for v in ["COLLECTION_NAME", "SHORTLINK_SITE", "SHORTLINK_API"] if not v]; sys.exit(f"Missing: {', '.join(missing)}") if missing else None
 
+# GLOBAL VERIFY FUNCTIONS 
 async def token_system_filter(_, __, message):
     uid = message.from_user.id
     if not VERIFY_EXPIRE or uid in PREMIUM_USERS:
@@ -40,6 +41,7 @@ async def global_verify_function(client, message):
                 return
     await send_verification(client, message)
         
+# DATABSE
 class VerifyDB():
     def __init__(self):
         self._dbclient = AsyncIOMotorClient(DATABASE_URL)
@@ -56,6 +58,7 @@ class VerifyDB():
 
 verifydb = VerifyDB()
 
+# FUNCTIONS
 async def is_user_verified(user_id):
     if not VERIFY_EXPIRE:
         return True
@@ -94,8 +97,6 @@ async def get_verify_token(bot, userid, link):
         token = ''.join(random.choices(string.ascii_letters + string.digits, k=9))
         long_link = f"{link}verify-{userid}-{token}"
         short_url = await get_short_url(long_link)
-        if DUAL_PROTECTION:
-            short_url = await Protection_url(short_url)
         vdict.update({'token': token, 'short_url': short_url})
     return short_url
 
@@ -121,11 +122,6 @@ async def get_short_url(longurl, shortener_site = SHORTLINK_SITE, shortener_api 
         print(e)
         return long_link
 
-async def Protection_url(short_url):
-    encoded_url = "".join(chr((ord(char) + 5) % 256) for char in short_url)
-    placeholder_link = "https://black-dot-redirect.vercel.app/decode?url="
-    return f"{placeholder_link}{quote(encoded_url)}"
-
 async def validate_token(client, message, data):
     user_id = message.from_user.id
     vdict = verify_dict.setdefault(user_id, {})
@@ -145,7 +141,7 @@ async def validate_token(client, message, data):
                             text=f'<b>Wᴇʟᴄᴏᴍᴇ Bᴀᴄᴋ 😁, Nᴏᴡ Yᴏᴜ Cᴀɴ Usᴇ Mᴇ Fᴏʀ {get_readable_time(VERIFY_EXPIRE)}.\n\n\nEɴᴊᴏʏʏʏ...❤️</b>',
                             reply_to_message_id=message.id,
                             )
-
+    
 def get_readable_time(seconds):
     periods = [('ᴅ', 86400), ('ʜ', 3600), ('ᴍ', 60), ('s', 1)]
     result = ''
