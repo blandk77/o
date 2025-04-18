f2d38d9d2512b31b60c9d53e651a8583e3c224a8
blandk77 <alexags1983@gmail.com>
2025-04-18 14:33:18 +0530

    Create addon.py

---
+++ helper/addon.py
@@ -0,0 +1,11 @@
+from config import Config
+async def send_log(b, u):
+    if Config.LOG_CHANNEL is not None:
+        botusername = await b.get_me()
+        curr = datetime.now(timezone("Asia/Kolkata"))
+        date = curr.strftime('%d %B, %Y')
+        time = curr.strftime('%I:%M:%S %p')
+        await b.send_message(
+            Config.LOG_CHANNEL,
+            f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: @{botusername.username}"
+        )
