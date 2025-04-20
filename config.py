2954cbb0f5048016556981b31dca0717713bbd64
blandk77 <alexags1983@gmail.com>
2025-04-20 23:52:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,21 @@ class Config(object):
     DB_URL  = os.environ.get("DB_URL", "mongodb+srv://telegramguy21:tnkIwvbNkJ5U3fZ7@botsuse.bpgag.mongodb.net/?retryWrites=true&w=majority&appName=Botsuse")  # ⚠️ Required
     DB_NAME  = os.environ.get("DB_NAME","SnowEnc") 
 
-    # Other Configs 
+    # ID's
     ADMIN = int(os.environ.get("ADMIN", "7465574522")) # ⚠️ Required
     LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002288135729')) # ⚠️ Required
     DUMP_CHANNEL = int(os.environ.get('DUMP_CHANNEL', '-1002288135729'))
+
+    #Nothing to fill in this line
     BOT_UPTIME = BOT_UPTIME  = time.time()
+
+    #Image to show in the Start command 
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
+    
+    #Fill if you want 4gb upload, else leave blank for 2gb upload 
     OWNER_STRING = os.environ.get("OWNER_STRING", "")
+
+    #start command buttons/credits
     UPDATES_BTN = os.environ.get("UPDATES_BTN", "BIDHAANBOTS") 
     DEV_BTN = os.environ.get("DEV_BTN", "BIG_FiiSH")
 
@@ -41,4 +49,3 @@ SHORTLINK_SITE = os.environ.get('SHORTLINK_SITE', 'linkmonetizer.in') # YOUR SHO
 SHORTLINK_API = os.environ.get('SHORTLINK_API', 'bf35b33b841943cdce510413393f35a9ff0bb558') # YOUR SHORTLINK API LIKE:- ma82owowjd9hw6_js7
 PREMIUM_USERS = list(map(int, os.environ.get('PREMIUM_USERS', '1705634892').split()))
 COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Verified') #For Token verification 
-DUAL_PROTECTION = bool(os.environ.get("DUAL_PROTECTION", True))
