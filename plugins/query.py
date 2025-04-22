f6f2b27fdf834c429f019b533b2f02132af8cebe
blandk77 <alexags1983@gmail.com>
2025-04-22 12:59:10 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -104,12 +104,15 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
             c_thumb = await db.get_thumbnail(query.from_user.id)
             dffmpeg = "-preset veryfast -c:v libx264 -s 840x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
             watermark = await db.get_watermark(query.from_user.id)
-            if watermark and watermark.startswith('-i '):
+        if watermark:
+            if watermark.startswith('-i '):
                 ffmpeg = f"{watermark} {dffmpeg.replace('-map 0', '')} -map 0:a? -map 0:s?"
-            if watermark and watermark.startswith('-vf '):
+            elif await db.get_custom_command_status(query.from_user.id):
                 ffmpeg = f"{watermark} {dffmpeg}"
             else:
-                ffmpeg = f"{dffmpeg}"
+                ffmpeg = f"{watermark} {dffmpeg}"
+        else:
+            ffmpeg = f"{dffmpeg}"
             await CompressVideo(bot=bot, query=query, ffmpegcode=ffmpeg, c_thumb=c_thumb)
         except Exception as e:
             print(e)
@@ -119,10 +122,15 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
             c_thumb = await db.get_thumbnail(query.from_user.id)
             dffmpeg = "-preset veryfast -c:v libx264 -s 1280x720 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
             watermark = await db.get_watermark(query.from_user.id)
-            if watermark:
+        if watermark:
+            if watermark.startswith('-i '):
+                ffmpeg = f"{watermark} {dffmpeg.replace('-map 0', '')} -map 0:a? -map 0:s?"
+            elif await db.get_custom_command_status(query.from_user.id):
                 ffmpeg = f"{watermark} {dffmpeg}"
             else:
-                ffmpeg = f"{dffmpeg}"
+                ffmpeg = f"{watermark} {dffmpeg}"
+        else:
+            ffmpeg = f"{dffmpeg}"
             await CompressVideo(bot=bot, query=query, ffmpegcode=ffmpeg, c_thumb=c_thumb)
         except Exception as e:
             print(e)
@@ -133,10 +141,15 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
             c_thumb = await db.get_thumbnail(query.from_user.id)
             dffmpeg = "-preset veryfast -c:v libx264 -s 1920x1080 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
             watermark = await db.get_watermark(query.from_user.id)
-            if watermark:
+        if watermark:
+            if watermark.startswith('-i '):
+                ffmpeg = f"{watermark} {dffmpeg.replace('-map 0', '')} -map 0:a? -map 0:s?"
+            elif await db.get_custom_command_status(query.from_user.id):
                 ffmpeg = f"{watermark} {dffmpeg}"
             else:
-                ffmpeg = f"{dffmpeg}"
+                ffmpeg = f"{watermark} {dffmpeg}"
+        else:
+            ffmpeg = f"{dffmpeg}"
             await CompressVideo(bot=bot, query=query, ffmpegcode=ffmpeg, c_thumb=c_thumb)
         except Exception as e:
             print(e)
@@ -147,10 +160,15 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
             c_thumb = await db.get_thumbnail(query.from_user.id)
             dffmpeg = "-preset veryfast -c:v libx264 -s 3840x2160 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5"
             watermark = await db.get_watermark(query.from_user.id)
-            if watermark:
+        if watermark:
+            if watermark.startswith('-i '):
+                ffmpeg = f"{watermark} {dffmpeg.replace('-map 0', '')} -map 0:a? -map 0:s?"
+            elif await db.get_custom_command_status(query.from_user.id):
                 ffmpeg = f"{watermark} {dffmpeg}"
             else:
-                ffmpeg = f"{dffmpeg}"
+                ffmpeg = f"{watermark} {dffmpeg}"
+        else:
+            ffmpeg = f"{dffmpeg}"
             await CompressVideo(bot=bot, query=query, ffmpegcode=ffmpeg, c_thumb=c_thumb)
         except Exception as e:
             print(e)
